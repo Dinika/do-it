811cf45068ab9ba65a5de981bad72d87d9311221
Dinika <dinikasaxenas@gmail.com>
2019-06-09 16:42:11 +0200

    innitialize project with typescript-eslint and prettier
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'react-app',
+        'plugin:prettier/recommended',
+    ],
+    plugins: ['@typescript-eslint', 'react'],
+    rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+    },
+};
